81d75554c8654f1f54d2a5d918cce650d88a23f8
Abdulmalik A. <amalikabdulmalik04@gmail.com>
2025-01-24 09:22:32 +0100

    feat:New feature - create user, result, and leaderboard entities.

---
+++ backend/src/result/enums/status.enum.ts
@@ -0,0 +1,4 @@
+export enum Status {
+  WON = 'won',
+  LOST = 'lost',
+}
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,35 @@
-export class User {}
+import { Leaderboard } from 'src/leaderboard/entities/leaderboard.entity';
+import { Result } from 'src/result/entities/result.entity';
+import {
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('varchar', { unique: true, nullable: false })
+  userName: string;
+
+  @Column('varchar', { unique: true, nullable: false })
+  email: string;
+
+  @Column('varchar', { nullable: false })
+  password: string;
+
+  @OneToMany(() => Result, (result) => result.user)
+  result: Result;
+
+  @OneToMany(() => Leaderboard, (leaderboard) => leaderboard.user)
+  leaderboard: Leaderboard;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
