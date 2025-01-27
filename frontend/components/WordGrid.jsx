4f4334277e5f0db6b2151879a53379fb72b0ba09
kamaldeen <kamalaliyu503@gmail.com>
2025-01-27 14:52:53 +0100

    box reduced

--- frontend/components/WordGrid.jsx
+++ frontend/components/WordGrid.jsx
@@ -4,14 +4,14 @@ function WordGrid() {
   const inputs = Array.from({ length: 25 });
 
   return (
-    <div className="mt-[150px]">
-      <div className="grid grid-cols-5 gap-2 md:gap-4 place-content-center mx-auto bg-white w-[250px] md:w-[500px] ">
+    <div className='mt-5'>
+      <div className="grid grid-cols-5 gap-2 md:gap-4 place-content-center mx-auto bg-white w-[250px] md:w-[350px] ">
         {inputs.map((_, index) => (
           <input
             key={index}
             type="text"
             maxLength="1"
-            className="w-[40px] h-[40px] md:w-[70px] md:h-[70px] rounded-[5px] bg-[#939B9F4D] text-center"
+            className="w-[40px] h-[40px] md:w-[60px] md:h-[60px] rounded-[5px] bg-[#939B9F4D] text-center"
           />
         ))}
       </div>
