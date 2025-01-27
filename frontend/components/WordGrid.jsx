2d56dbb608d19331e12dcd4f0cd78c98ce6032d0
kamaldeen <kamalaliyu503@gmail.com>
2025-01-27 13:59:15 +0100

    Response adjusted

--- frontend/components/WordGrid.jsx
+++ frontend/components/WordGrid.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 function WordGrid() {
-  
   const inputs = Array.from({ length: 25 });
 
   return (
     <div className="mt-[150px]">
-      <div className="grid grid-cols-5 gap-2 sm:overflow-auto gap-x-2 gap-y-2 md:gap-x-2 bg-[#FFFF] p-4">
+      <div className="grid grid-cols-5 gap-2 md:gap-4 place-content-center mx-auto bg-white w-[250px] md:w-[500px] ">
         {inputs.map((_, index) => (
           <input
             key={index}
             type="text"
             maxLength="1"
-            className="h-[55px] w-[55px] md:h-[70px] md:w-[70px] rounded-[5px] bg-[#939B9F4D] text-center"
+            className="w-[40px] h-[40px] md:w-[70px] md:h-[70px] rounded-[5px] bg-[#939B9F4D] text-center"
           />
         ))}
       </div>
