a050eb13a2bf264cbf4c891fdd31081a3f3d2383
kamaldeen <kamalaliyu503@gmail.com>
2025-01-27 09:22:19 +0100

    FIX: resolve conflit

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
 import Keyboard from "@/components/Keyboard.jsx";
 import Navbar from "@/components/Navbar";
+import WordGrid from "@/components/WordGrid";
 export default function page() {
   return (
     <div>
     <Navbar/>
+    <WordGrid/>
     <Keyboard />
     </div>
   );
