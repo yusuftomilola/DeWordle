5126267f7de37803b644cd17cc066367752bba32
Assad Isah <nottherealalanturing@gmail.com>
2025-01-28 16:01:37 +0100

    added dtos

--- backend/src/leaderboard/dto/create-leaderboard.dto.ts
+++ backend/src/leaderboard/dto/create-leaderboard.dto.ts
@@ -1,1 +1,18 @@
-export class CreateLeaderboardDto {}
+import { IsInt, IsNumber, IsOptional } from 'class-validator';
+export class CreateLeaderboardDto {
+  @IsInt()
+  @IsOptional()
+  readonly id?: number;
+
+  @IsInt()
+  readonly userId: number;
+
+  @IsInt()
+  readonly totalWins: number;
+
+  @IsInt()
+  readonly totalAttempts: number;
+
+  @IsNumber()
+  readonly averageScore: number;
+}
--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  Matches,
+  MaxLength,
+} from 'class-validator';
+import { Result } from 'src/result/entities/result.entity';
+import { Column } from 'typeorm';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  userName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MaxLength(225)
+  @Matches(
+    /^(?=.*[!@#$%^&])(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{8,16}$/,
+    {
+      message:
+        'Password must include at least one uppercase letter, one lowercase letter, one number, and one special character.',
+    },
+  )
+  password: string;
+
+  @Column()
+  result: Result;
+}
