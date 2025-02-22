16ad7e331880284dab6ccfa54d53d1a9a3d61cfb
portable <emmanueltemitopedorcas20@gmail.com>
2025-02-22 20:06:50 +0100

    "fix: the app.module"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,20 +6,9 @@ import { AuthModule } from './auth/auth.module';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
 import { AdminModule } from './admin/admin.module';
 import { ResultModule } from './result/result.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost', // Change to your PostgreSQL host
-      port: 5432,
-      username: 'yourusername', // Replace with your PostgreSQL username
-      password: 'yourpassword', // Replace with your PostgreSQL password
-      database: 'yourdatabase', // Replace with your database name
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Auto-detect entity files
-      synchronize: true, // Set to false in production
-    }),
     UsersModule,
     AuthModule,
     LeaderboardModule,
