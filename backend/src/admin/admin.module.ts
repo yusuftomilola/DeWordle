e7d42a8cfe45a93268809cc5e2f64ee2a52a56f0
Ibinola <ibinolamichael1@gmail.com>
2025-02-24 20:57:56 +0100

    fix: resolve AdminRepository injection issue in AdminService

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Admin } from './entities/admin.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Admin])],
   controllers: [AdminController],
   providers: [AdminService],
 })
