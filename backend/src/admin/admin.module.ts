2c8111628e0b89b6e66caeb7b704d66853c22e4f
Ibinola <ibinolamichael1@gmail.com>
2025-02-25 11:17:10 +0100

    remove duplicate identifier

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -2,11 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
-
-import { Admin } from './entities/admin.entity';
-
-
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin } from './entities/admin.entity';
 
 @Module({
