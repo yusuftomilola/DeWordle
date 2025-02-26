d5f1dc8811ac20c1a97823ed7c7918ae43d6e774
phertyameen <aminubabafatima8@gmail.com>
2025-02-26 20:55:46 +0100

    feat:socialauth

---
+++ backend/src/auth/social/interfaces/user.interface.ts
@@ -0,0 +1,6 @@
+export interface GoogleInterface {
+    email: string
+    firstName: string
+    lastName: string
+    googleId: string
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,15 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   // enable cors
+   app.enableCors({
+    origin: 'http://localhost:3500/', // All locations
+    credentials: true, // Allow cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
