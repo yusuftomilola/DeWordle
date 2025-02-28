f3f827b8668fc0494d2b3097c084f9bff6a49db6
DIVINE IFEDIORAH <ifediorahchinonso5@gmail.com>
2025-02-28 15:10:47 +0100

    coming soon feature

---
+++ frontend/app/(root)/coming-soon/page.jsx
@@ -0,0 +1,12 @@
+import ComingSoon from '../../../components/ComingSoon'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <ComingSoon   />
+    </div>
+  )
+}
+
+export default page
