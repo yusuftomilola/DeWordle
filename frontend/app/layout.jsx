45373914ceb91085f070a43ff42eccbfb0f5b527
Assad Isah <nottherealalanturing@gmail.com>
2025-03-21 14:34:21 +0100

    hooking up the auth route for signup

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,30 +1,31 @@
-"use client";
+'use client';
 
-import { Geist, Azeret_Mono as Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { metadata } from "./metadata";
-import { ThemeProvider } from "../context/ThemeContext";
-import { Manrope } from "next/font/google";
-import { Roboto } from "next/font/google";
+import { Geist, Azeret_Mono as Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { metadata } from './metadata';
+import { ThemeProvider } from '../context/ThemeContext';
+import { Manrope } from 'next/font/google';
+import { Roboto } from 'next/font/google';
+import { QueryProvider, SessionProvider } from '@/app/providers';
 
 const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-roboto",
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
 });
 const manrope = Manrope({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-manrope",
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-manrope',
 });
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export default function RootLayout({ children }) {
@@ -39,7 +40,9 @@ export default function RootLayout({ children }) {
           <meta name="description" content={metadata.description} />
         </head>
         <body className="min-h-screen flex flex-col justify-between h-auto w-full antialiased">
-          <main className="flex-grow ">{children}</main>
+          <QueryProvider>
+            <main className="flex-grow ">{children}</main>
+          </QueryProvider>
         </body>
       </html>
     </ThemeProvider>
