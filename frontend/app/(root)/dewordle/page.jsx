8c03e492e023295b594f8cacc5d27d2dcb9974d5
Tinna23 <tylermalcom31@gmail.com>
2025-03-23 13:12:04 +0100

    Feat: Implement Dynanmic Nav & Global footer

---
+++ frontend/app/(root)/dewordle/page.jsx
@@ -0,0 +1,12 @@
+import Dewordle from '@/components/Dewordle'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Dewordle/>
+    </div>
+  )
+}
+
+export default page
