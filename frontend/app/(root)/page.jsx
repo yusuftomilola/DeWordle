c208bfebb02efafa16dbd48ae457e7ed4e1a1d66
JayWebtech <jethroadamzy@gmail.com>
2025-03-24 16:35:16 +0100

    feat : what's new and most played component

--- frontend/app/(root)/page.jsx
+++ frontend/app/(root)/page.jsx
@@ -1,13 +1,17 @@
 
 
 import LandingPage from "@/components/LandingPage";
+import MostPlayed from "@/components/MostPlayed";
 import PlayOurGame from "@/components/playourgame";
+import WhatsNew from "@/components/WhatsNew";
 
 export default function Page() {
   return (
     <div className="bg-[#ffffff]">
       <LandingPage />
       <PlayOurGame />
+      <WhatsNew />
+      <MostPlayed />
     </div>
   );
 }
