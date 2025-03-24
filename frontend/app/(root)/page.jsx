f4f37e3507b463e2dd0d8503ba5e52fddb0e7e33
clintjeff2 <clintjeff2@gmail.com>
2025-03-24 22:03:48 +0100

    Added trending section in main page

--- frontend/app/(root)/page.jsx
+++ frontend/app/(root)/page.jsx
@@ -1,13 +1,13 @@
-
-
-import { HeroSlider } from "@/components/HeroSlider";
-import PlayOurGame from "@/components/playourgame";
+import { HeroSlider } from '@/components/HeroSlider';
+import PlayOurGame from '@/components/playourgame';
+import TrendingGames from '@/components/TrendingGames';
 
 export default function Page() {
-  return (
-    <div className="bg-[#ffffff]">
-      <HeroSlider />
-      <PlayOurGame />
-    </div>
-  );
+	return (
+		<div className="bg-[#ffffff]">
+			<HeroSlider />
+			<PlayOurGame />
+			<TrendingGames />
+		</div>
+	);
 }
