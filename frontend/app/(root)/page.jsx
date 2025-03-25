59bdda11a1ed7fd234042d670910bb880ecbb51d
Luluameh <amehajumi3@gmail.com>
2025-03-25 17:26:07 +0100

    feat: Implement skeletal layout loader for landing page

--- frontend/app/(root)/page.jsx
+++ frontend/app/(root)/page.jsx
@@ -1,18 +1,34 @@
-
-import { HeroSlider } from '@/components/HeroSlider';
+"use client";
+import { useState, useEffect } from "react";
+import { HeroSlider } from "@/components/HeroSlider";
 import MostPlayed from "@/components/MostPlayed";
-import PlayOurGame from '@/components/playourgame';
-import TrendingGames from '@/components/TrendingGames';
+import PlayOurGame from "@/components/playourgame";
+import TrendingGames from "@/components/TrendingGames";
 import WhatsNew from "@/components/WhatsNew";
+import { LandingPageSkeletonLoader } from "@/components/ui/LandingPageSkeletonLoader";
 
 export default function Page() {
-	return (
-		<div className="bg-[#ffffff]">
-			<HeroSlider />
-			<PlayOurGame />
-			<TrendingGames />
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <LandingPageSkeletonLoader />;
+  }
+
+  return (
+    <div className="bg-[#ffffff]">
+      <HeroSlider />
+      <PlayOurGame />
+      <TrendingGames />
       <WhatsNew />
       <MostPlayed />
-		</div>
-	);
+    </div>
+  );
 }
