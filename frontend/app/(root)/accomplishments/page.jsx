180bd348c2b26a000ebeab137b93323d736bc49e
shamoo53 <aliyushamsu16@gmail.com>
2025-03-26 11:06:03 +0100

    implementation of streak-accomplishments-system

--- frontend/app/(root)/accomplishments/page.jsx
+++ frontend/app/(root)/accomplishments/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Accomplishments from "@/components/Accomplishments";
 const page = () => {
   return (
     <div>
-      
+      <Accomplishments />
     </div>
   );
 };
