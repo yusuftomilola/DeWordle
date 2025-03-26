3807437ba314e093dd24f083852a9b8a0ce0ab5a
Ibinola <ibinolamichael1@gmail.com>
2025-03-26 02:18:36 +0100

    add spelling-bee page placeholder

---
+++ frontend/app/(root)/spelling-bee/page.jsx
@@ -0,0 +1,12 @@
+import SpellingBee from '@/components/SpellingBee'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <SpellingBee />
+    </div>
+  )
+}
+
+export default page
---
+++ frontend/components/SpellingBee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const SpellingBee = () => { 
+    return (
+        <div>
+            <h1>Spelling Bee</h1>
+        </div>
+    )
+}
+
+export default SpellingBee
