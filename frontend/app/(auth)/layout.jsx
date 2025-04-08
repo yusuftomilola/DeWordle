e477227529e2e0d97afc3e3cdc2abde9a19ed533
BigBen7 <103938678+BigBen-7@users.noreply.github.com>
2025-04-08 12:32:52 +0100

    fix: remove interface from Auth layout

--- frontend/app/(auth)/layout.jsx
+++ frontend/app/(auth)/layout.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
-interface AuthLayoutProps {
-  children: React.ReactNode;
-}
 
-const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+const AuthLayout = ({ children }) => {
   return (
     <div className="bg-[#ffffff] flex flex-col">
       <main className="">{children}</main>
