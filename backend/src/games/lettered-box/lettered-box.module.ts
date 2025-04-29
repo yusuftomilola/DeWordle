fe7d7c6384e789a9edf2b67d43eb3ed2b0b5a263
khadijatoyawoye <88722194+khadijatoyawoye@users.noreply.github.com>
2025-04-29 15:17:09 +0100

    define dto's and entity for lettered box game

--- backend/src/games/lettered-box/lettered-box.module.ts
+++ backend/src/games/lettered-box/lettered-box.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LetteredBoxService } from './lettered-box.service';
 import { LetteredBoxController } from './lettered-box.controller';
+import { LetteredBoxEntity } from './entities/lettered-box.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LetteredBoxEntity])],
   controllers: [LetteredBoxController],
   providers: [LetteredBoxService],
 })
