5ded367ce6f455d8b9dde81c63ec31a74715f45e
TInna23 <tylermalcom31@gmail.com>
2025-04-29 22:21:36 +0100

    feat(lettered-box): implement game logic and API routes for solution submission and leaderboard

--- backend/src/games/lettered-box/lettered-box.controller.ts
+++ backend/src/games/lettered-box/lettered-box.controller.ts
@@ -1,34 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { LetteredBoxService } from './lettered-box.service';
-import { CreateLetteredBoxDto } from './dto/create-lettered-box.dto';
-import { UpdateLetteredBoxDto } from './dto/update-lettered-box.dto';
+
+
+import { SubmitSolutionDto } from './dto/submit-solution-dto';
+import { JwtAuthGuard } from 'security/guards/jwt-auth.guard';
 
 @Controller('lettered-box')
 export class LetteredBoxController {
   constructor(private readonly letteredBoxService: LetteredBoxService) {}
 
-  @Post()
-  create(@Body() createLetteredBoxDto: CreateLetteredBoxDto) {
-    return this.letteredBoxService.create(createLetteredBoxDto);
+  @UseGuards(JwtAuthGuard)
+  @Post('submit')
+  async submit(@Body() dto: SubmitSolutionDto, @Request() req): Promise<any> {
+    const userId = req.user.id;
+    return this.letteredBoxService.submitSolution(dto, userId);
   }
 
-  @Get()
-  findAll() {
-    return this.letteredBoxService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.letteredBoxService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLetteredBoxDto: UpdateLetteredBoxDto) {
-    return this.letteredBoxService.update(+id, updateLetteredBoxDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.letteredBoxService.remove(+id);
+  @Get('leaderboard')
+  async leaderboard() {
+    return this.letteredBoxService.getLeaderboard();
   }
 }
