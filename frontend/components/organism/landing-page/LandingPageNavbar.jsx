f00e7feba6328c76eca44968101c911b6ce47432
Akshola00 <akinniyishola07@gmail.com>
2025-05-02 18:39:01 +0100

    Add ArgentInvisibleWalletConnect component and update environment variables

--- frontend/components/organism/landing-page/LandingPageNavbar.jsx
+++ frontend/components/organism/landing-page/LandingPageNavbar.jsx
@@ -14,6 +14,7 @@ import {
 import { AppContext } from "@/context/AppContext";
 import { motion, AnimatePresence } from "framer-motion";
 import { Settings } from "./Settings";
+import { ArgentInvisibleWalletButton } from "./ArgentInvisibleWalletConnect";
 
 
 export default function LandingPageNavbar() {
@@ -267,6 +268,7 @@ export default function LandingPageNavbar() {
           >
             New
           </Link>
+          <ArgentInvisibleWalletButton />
           {/* mobile auth buttons/Users */}
           <div className="flex flex-col space-y-3 pt-2">
             {!isLoggedIn ? (
