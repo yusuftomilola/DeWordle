2fc11a8e4aee2177826e0341f88f5887ce59a3e0
clintjeff2 <clintjeff2@gmail.com>
2025-06-09 01:00:49 +0100

    Fixed typescript compile time error, added DictionaryModule to list of imports for the puzzle module, also added PuzzleSession to list of typeorm imports for the puzzle module

--- backend/src/puzzle/puzzle.module.ts
+++ backend/src/puzzle/puzzle.module.ts
@@ -1,18 +1,30 @@
-import { Module } from "@nestjs/common"
-import { PuzzleService } from "./puzzle.service"
-import { PuzzleController, AdminPuzzleController } from "./puzzle.controller"
-import { TypeOrmModule } from "@nestjs/typeorm"
-import { Puzzle } from "./entities/puzzle.entity"
-import { IsGridValid } from "./validators/grid.validator"
-import { IsSpangramValid } from "./validators/spangram.validator"
-import { IsDateUnique } from "./validators/date-unique.validator"
-import { AreValidWordsValid } from "./validators/valid-words.validator"
-import { IsFutureDate } from "./validators/feature-date.validator"
+import { Module } from '@nestjs/common';
+import { PuzzleService } from './puzzle.service';
+import { PuzzleController, AdminPuzzleController } from './puzzle.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Puzzle } from './entities/puzzle.entity';
+import { IsGridValid } from './validators/grid.validator';
+import { IsSpangramValid } from './validators/spangram.validator';
+import { IsDateUnique } from './validators/date-unique.validator';
+import { AreValidWordsValid } from './validators/valid-words.validator';
+import { IsFutureDate } from './validators/feature-date.validator';
+import { PuzzleSession } from './entities/puzzle-session.entity';
+import { DictionaryModule } from 'src/dictionary/dictionary.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Puzzle])],
+  imports: [
+    TypeOrmModule.forFeature([Puzzle, PuzzleSession]),
+    DictionaryModule,
+  ],
   controllers: [PuzzleController, AdminPuzzleController],
-  providers: [PuzzleService, IsGridValid, IsSpangramValid, IsDateUnique, IsFutureDate, AreValidWordsValid],
+  providers: [
+    PuzzleService,
+    IsGridValid,
+    IsSpangramValid,
+    IsDateUnique,
+    IsFutureDate,
+    AreValidWordsValid,
+  ],
   exports: [PuzzleService],
 })
 export class PuzzleModule {}
