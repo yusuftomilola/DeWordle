dfe7f04f8b265dba62752924ab8536d4b60f416b
Fahmed2024 <100038212+Fahmedo@users.noreply.github.com>
2025-07-28 15:43:40 +0100

    commit new setup and authentication setup

--- frontend/utils/schema/forgotPasswordSchema.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const ForgotPasswordSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Invalid email address")
-    .required("Email is required"),
-});
