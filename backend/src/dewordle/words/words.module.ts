11f57205300bf6b18a6feb6daf0d8b8699dc8b13
clintjeff2 <clintjeff2@gmail.com>
2025-07-28 14:12:30 +0100

    feat: export WordValidationService in WordsModule

--- backend/src/dewordle/words/words.module.ts
+++ backend/src/dewordle/words/words.module.ts
@@ -6,11 +6,18 @@ import { Word } from '../../entities/word.entity';
 import { WordSeedService } from '../../utils/word-seed.service';
 import { WordScheduler } from './word.scheduler';
 import { WordScoringProvider } from './providers/word-scoring-provider';
+import { WordValidationService } from './word-validation.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Word])],
   controllers: [WordsController],
-  providers: [WordsService, WordSeedService, WordScheduler, WordScoringProvider],
-  exports: [WordsService, WordSeedService],
+  providers: [
+    WordsService,
+    WordSeedService,
+    WordScheduler,
+    WordScoringProvider,
+    WordValidationService,
+  ],
+  exports: [WordsService, WordSeedService, WordValidationService],
 })
 export class WordsModule {}
